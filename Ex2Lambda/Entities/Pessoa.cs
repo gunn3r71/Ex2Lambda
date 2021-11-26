e9686302e3c60187c8fda3a73b1f629a2f3934ad
Lucas <lucasgunner3@gmail.com>
2021-11-25 21:04:09 -0300

    Adicionar arquivos de projeto.

---
+++ Ex2Lambda/Entities/Pessoa.cs
@@ -0,0 +1,21 @@
+﻿using static Ex2Lambda.Program;
+
+namespace Ex2Lambda.Entities
+{
+    public class Pessoa
+    {
+        public Pessoa(double peso, double altura)
+        {
+            Peso = peso;
+            Altura = altura;
+        }
+
+        public double Peso { get; private set; }
+        public double Altura { get; private set; }
+
+        public double CalcularImc(Calculo calculo)
+        {
+            return calculo(Peso, Altura);
+        }
+    }
+}
---
+++ Ex2Lambda/Program.cs
@@ -0,0 +1,19 @@
+﻿using Ex2Lambda.Entities;
+using System;
+
+namespace Ex2Lambda
+{
+    public class Program
+    {
+        public delegate double Calculo(double x, double y);
+
+        static void Main(string[] args)
+        {
+            var pessoa = new Pessoa(80, 1.70);
+
+            var imc = pessoa.CalcularImc((x, y) => x / Math.Pow(y, 2));
+
+            Console.WriteLine(imc);
+        }
+    }
+}
